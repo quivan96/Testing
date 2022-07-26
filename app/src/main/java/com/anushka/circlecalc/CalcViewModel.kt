9a251bdea98e449595aae7746a84a2f14e172d26
Qui Van <quivan3396@yahoo.com>
2022-07-26 14:07:49 -0400

    initial tests

---
+++ app/src/main/java/com/anushka/circlecalc/CalcViewModel.kt
@@ -0,0 +1,53 @@
+package com.anushka.circlecalc
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.lang.Exception
+
+class CalcViewModel(
+    private val calculations: Calculations
+) : ViewModel() {
+
+    var radius = MutableLiveData<String>()
+
+    var _area = MutableLiveData<String?>()
+    val areaValue = _area
+
+    var _circumference = MutableLiveData<String?>()
+    val circumferenceValue = _circumference
+
+
+    fun calculate() {
+
+        try {
+
+            val radiusDoubleValue = radius.value?.toDouble()
+            if (radiusDoubleValue != null) {
+                calculateArea(radiusDoubleValue)
+                calculateCircumference(radiusDoubleValue)
+            } else {
+                _area.value = null
+                _circumference.value = null
+            }
+
+        } catch (e: Exception) {
+            Log.i("MYTAG", e.message.toString())
+            _area.value = null
+            _circumference.value = null
+        }
+
+    }
+
+    fun calculateArea(radius: Double) {
+        val calculatedArea = calculations.calculateArea(radius)
+        _area.value = calculatedArea.toString()
+    }
+
+    fun calculateCircumference(radius: Double) {
+        val calculatedCircumference = calculations.calculateCircumference(radius)
+        _circumference.value = calculatedCircumference.toString()
+    }
+
+}
---
+++ app/src/test/java/com/anushka/circlecalc/MyCalcTest.kt
@@ -0,0 +1,44 @@
+package com.anushka.circlecalc
+
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+
+class MyCalcTest {
+
+    private lateinit var myCalc: MyCalc
+
+    @Before
+    fun setUp() {
+        myCalc = MyCalc()
+    }
+    @Test
+    fun calculateCircumference_radiusGiven_returnsCorrectResult() {
+        val result = myCalc.calculateCircumference(2.1)
+
+        assertThat(result).isEqualTo(13.188)
+    }
+
+    @Test
+    fun calculateCircumference_zeroRadius_returnsCorrectResult() {
+        val result = myCalc.calculateCircumference(0.0)
+
+        assertThat(result).isEqualTo(0.0)
+    }
+
+    @Test
+    fun calculateArea_givenArea_returnsCorrectResult() {
+        val result = myCalc.calculateArea(5.0)
+
+        assertThat(result).isEqualTo(78.5)
+    }
+
+    @Test
+    fun calculateArea_zeroArea_returnsCorrectResult() {
+        val result = myCalc.calculateArea(0.0)
+
+        assertThat(result).isEqualTo(0.0)
+    }
+}
